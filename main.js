b8a3a36b559a0635625b89c7899da483d98bc091
Tim Liederbach <Timmy@Tims-MacBook-Pro-2.local>
2018-03-03 10:44:13 -0500

    created game logic from unit 9

--- main.js
+++ main.js
@@ -1,22 +1,29 @@
 var cards = ["queen" , "queen" , "king" , "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
+var checkForMatch = function () {
 
-cardsInPlay.push(cardOne);
-
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2)	{
-	if (cardsInPlay[0] === cardsInPlay[1])	{
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
-		}
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
 }
 
+};
 
+var flipCard = function (cardId) {
+
+console.log("User flipped" + cards[cardId]);
+cardsInPlay.push(cards[cardId])
+
+if (cardsInPlay.length === 2)	{
+	
+checkForMatch ();
+
+}
+};
+
+flipCard(0);
+flipCard(2);
 
 
